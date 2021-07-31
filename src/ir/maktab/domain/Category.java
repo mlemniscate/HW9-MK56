fdb286386573712159ae24bcba0535a80740b388
Milad Abdi <miladabdi1374@gmail.com>
2021-07-31 09:28:33 +0430

    Add Category

---
+++ src/ir/maktab/domain/Category.java
@@ -0,0 +1,37 @@
+package ir.maktab.domain;
+
+import ir.maktab.base.domain.BaseEntity;
+
+import java.util.List;
+
+public class Category<T extends Category<T>> extends BaseEntity<Integer> {
+
+    private List<T> categories;
+    private T parent;
+
+    public Category(List<T> categories, T parent) {
+        this(0, categories, parent);
+    }
+
+    public Category(Integer id, List<T> categories, T parent) {
+        super(id);
+        this.categories = categories;
+        this.parent = parent;
+    }
+
+    public List<T> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<T> categories) {
+        this.categories = categories;
+    }
+
+    public T getParent() {
+        return parent;
+    }
+
+    public void setParent(T parent) {
+        this.parent = parent;
+    }
+}
