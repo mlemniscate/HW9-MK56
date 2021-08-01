614e62097ceb82e2db45c36be7735d7e2f4caf1a
Milad Abdi <miladabdi1374@gmail.com>
2021-08-01 10:40:05 +0430

    Add MainApp

---
+++ src/ir/maktab/java_second/MainApp.java
@@ -0,0 +1,9 @@
+package ir.maktab.java_second;
+
+import java.util.LinkedList;
+
+public class MainApp {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>();
+    }
+}
